2fd4b9598501159652318f0556520e7583da85f5
leihan <han.lei@mail.hypers.com>
2020-05-26 14:58:27 +0800

    feat: 🎸 新增babel配置项

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [], 
+  presets: [
+    [
+      'remax',
+      {
+        typescript: {
+          allowNamespaces: true,
+        },
+        'class-properties': {
+          loose: true,
+        },
+        decorators: {
+          legacy: true,
+        },
+        'throw-if-namespace': true,
+        targets: ['chrome >= 49', 'firefox >= 64', 'ios >= 8', 'Android > 4.4'],
+      },
+    ],
+  ],
+};
